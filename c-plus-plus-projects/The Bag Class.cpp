799e100721a96b9a68c4d13b56ae2d5bcb5d06ea
Jeron Davidson <36479762+jeron95@users.noreply.github.com>
2018-03-24 02:16:42 -0400

    Update The Bag Class.cpp

--- c-plus-plus-projects/The Bag Class.cpp
+++ c-plus-plus-projects/The Bag Class.cpp
@@ -14,81 +14,51 @@
 using namespace std;
 
 class bag
-
-    {
-
+{
     public:
 
        // TYPEDEFS and MEMBER CONSTANTS
-
-
-
         typedef string value_type;
-
         typedef size_t size_type;
-
         static const size_type CAPACITY = 1000;        
         // CONSTRUCTOR
-
         bag( ) { used = 0; }
 
         // MODIFICATION MEMBER FUNCTIONS
-
         size_type erase(const value_type& target);
-
         bool erase_one(const value_type& target);
-
         void insert(const value_type& entry);
 
         // CONSTANT MEMBER FUNCTIONS
-
         size_type size( ) const{ return used; }
-
         size_type count(const value_type& target) const;
-
+        
     private:
-
-        value_type data[CAPACITY];  // The array to store items
-
-        size_type used;             // How much of array is used
+    
+    value_type data[CAPACITY];  // The array to store items
+    size_type used;             // How much of array is used
 
     };
 
-
-
     bag::size_type bag::erase(const value_type& target)
+    {
+        size_type index = 0;
+        size_type many_removed = 0;
+    while(index < used)
+    {
+        if(data[index] == target)
 
     {
+            --used;
+        data[index] = data[used];
+        ++many_removed;
 
-     size_type index = 0;
+    }
+    else
+    ++index;
 
-     size_type many_removed = 0;
-
- 
-
-     while(index < used)
-
-     {
-
-         if(data[index] == target)
-
-         {
-
-           --used;
-
-           data[index] = data[used];
-
-           ++many_removed;
-
-         }
-
-         else
-
-           ++index;
-
-     }
-
-     return many_removed;
+    }
+        return many_removed;
 
     }
 
@@ -96,9 +66,7 @@ class bag
 
     {
 
-     size_type index; // The location of target in the data array  
-
- 
+    size_type index; // The location of target in the data array  
 
      // First, set index to the location of target in the data array,
 
@@ -112,65 +80,36 @@ class bag
      {
          ++index;
      }
-
-
-     if(index == used)
+    if(index == used)
      {
-         return false; // target isn’t in the bag, so no work to do.
+        return false; // target isn’t in the bag, so no work to do.
      
      }
 
      // When execution reaches here, target is in the bag at data[index].
-
-     // So, reduce used by 1 and copy the last item onto data[index].
-
-     --used;
-
-     data[index] = data[used]; 
-     
-
-     return true;
-
+    // So, reduce used by 1 and copy the last item onto data[index].
+        --used;
+    data[index] = data[used]; 
+    return true;
     }
-
- 
 
     void bag::insert(const value_type& entry)
-
     { 
-
         assert(size( ) < CAPACITY);
-
- 
-
         data[used] = entry;
-
-                   ++used;
-
+        ++used;
     }
 
- 
-
     bag::size_type bag::count(const value_type& target) const
-
     {
-
         size_type answer;
-
         size_type i;
-
- 
-
         answer = 0;
 
         for(i = 0; i < used; ++i)
-
             if(target == data[i])
-
-                ++answer;
-
+        ++answer;
         return answer;
-
     }
     
     int main()
